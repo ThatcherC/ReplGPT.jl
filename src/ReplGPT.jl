6c2e22d6cf9abce1889e89d13b56fd6d1f2f2be0
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-03-07 17:33:41 -0500

    Add checks for missing API key

--- src/ReplGPT.jl
+++ src/ReplGPT.jl
@@ -4,23 +4,37 @@ import OpenAI
 import ReplMaker
 import Markdown
 
-function call_chatgpt(s)
-    r = OpenAI.create_chat(
-        ENV["OPENAI_API_KEY"],
-        "gpt-3.5-turbo",
-        [Dict("role" => "user", "content" => s)],
-    )
+const apiKeyName = "OPENAI_API_KEY"
 
-    # TODO: check for errors!
-    #if !=(r.status, 200)
-    #  @test false
-    #end  
-    response = r.response["choices"][begin]["message"]["content"]
-    # TODO: check if response is valid markdown. if not, return as string
-    Markdown.parse(response)
+function call_chatgpt(s)
+
+    if haskey(ENV, apiKeyName)
+        r = OpenAI.create_chat(
+            ENV[apiKeyName],
+            "gpt-3.5-turbo",
+            [Dict("role" => "user", "content" => s)],
+        )
+
+        # TODO: check for errors!
+        #if !=(r.status, 200)
+        #  @test false
+        #end  
+        response = r.response["choices"][begin]["message"]["content"]
+
+        Markdown.parse(response)
+    else
+        Markdown.parse(
+            "No API key found in ENV! Please set the OpenAI API key environment variable with $(apiKeyName)=<YOUR OPENAI API KEY>",
+        )
+    end
 end
 
 function init_repl()
+
+    if !haskey(ENV, apiKeyName)
+        @warn "$apiKeyName not found in ENV! Please set the environment variable $(apiKeyName)=<YOUR OPENAI API KEY>"
+    end
+
     ReplMaker.initrepl(
         call_chatgpt,
         prompt_text = "ChatGPT> ",
