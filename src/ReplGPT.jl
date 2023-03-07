abdf7efa9d37c698041f5cbf8b42539819ca627a
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-03-07 17:22:49 -0500

    Formatting pass

--- src/ReplGPT.jl
+++ src/ReplGPT.jl
@@ -5,27 +5,29 @@ import ReplMaker
 import Markdown
 
 function call_chatgpt(s)
-  r = OpenAI.create_chat(
-    ENV["OPENAI_API_KEY"], 
-    "gpt-3.5-turbo",
-    [Dict("role" => "user", "content"=> s)]
-  )
+    r = OpenAI.create_chat(
+        ENV["OPENAI_API_KEY"],
+        "gpt-3.5-turbo",
+        [Dict("role" => "user", "content" => s)],
+    )
 
-  # TODO: check for errors!
-  #if !=(r.status, 200)
-  #  @test false
-  #end  
-  response = r.response["choices"][begin]["message"]["content"]
-  # TODO: check if response is valid markdown. if not, return as string
-  Markdown.parse(response)
+    # TODO: check for errors!
+    #if !=(r.status, 200)
+    #  @test false
+    #end  
+    response = r.response["choices"][begin]["message"]["content"]
+    # TODO: check if response is valid markdown. if not, return as string
+    Markdown.parse(response)
 end
 
 function init_repl()
-  ReplMaker.initrepl(call_chatgpt, 
-                prompt_text="ChatGPT> ",
-                prompt_color = :blue, 
-                start_key='}', 
-                mode_name="ChatGPT_mode");
+    ReplMaker.initrepl(
+        call_chatgpt,
+        prompt_text = "ChatGPT> ",
+        prompt_color = :blue,
+        start_key = '}',
+        mode_name = "ChatGPT_mode",
+    )
 end
 
 __init__() = isdefined(Base, :active_repl) ? init_repl() : nothing
