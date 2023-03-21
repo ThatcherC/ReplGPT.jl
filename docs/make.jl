c7ce24fead3799ec1d588fde87f888da043e3866
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-03-20 22:42:02 -0400

    Formatting pass

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,9 @@
 using Documenter
 using ReplGPT
 
-makedocs(
-    sitename = "ReplGPT",
-    format = Documenter.HTML(),
-    modules = [ReplGPT]
-)
+makedocs(sitename = "ReplGPT", format = Documenter.HTML(), modules = [ReplGPT])
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(
-    repo = "https://github.com/ThatcherC/ReplGPT.jl.git"
-)
+deploydocs(repo = "https://github.com/ThatcherC/ReplGPT.jl.git")
