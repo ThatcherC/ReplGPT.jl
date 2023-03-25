95c54b908a388161658af9c40cc887e82242d697
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-03-25 12:31:37 -0400

    Added conversation-related functions

--- src/ReplGPT.jl
+++ src/ReplGPT.jl
@@ -9,7 +9,108 @@ using Preferences
 include("formatting.jl")
 include("keys.jl")
 
-conversation = Vector{Dict{String,String}}()
+"""
+    function ReplGPT.generate_empty_conversation()
+
+Returns a new empty conversation history.
+"""
+function generate_empty_conversation()
+    Vector{Dict{String,String}}();
+end
+
+conversation = generate_empty_conversation();
+
+"""
+    function ReplGPT.initialize_conversation()
+
+Sets the ChatGPT conversation to an empty state. This effectively
+starts a new chat with ChatGPT with no recollection of past messages.
+"""
+function initialize_conversation()
+    global conversation = generate_empty_conversation();
+end
+
+"""
+    function ReplGPT.conversation_as_string()
+
+Serializes a conversation history to a string. Messages sent from the user
+are preceded by "You:", messages from ChatGPT are preceded by "ChatGPT:",
+and any system messages are preceded by "System:".
+
+"""
+function conversation_as_string()
+    buf = IOBuffer()
+    for message in conversation
+        if message["role"] == "user"
+            println(buf, "You:")
+            println(buf, "----\n")
+        elseif message["role"] == "assistant"
+            println(buf, "ChatGPT:")
+            println(buf, "--------\n")
+        elseif message["role"] == "system"
+            println(buf, "System:")
+            println(buf, "-------\n")
+        end
+
+        println(buf, message["content"])
+        println(buf)
+    end
+
+    String(take!(buf))
+end
+
+"""
+    function ReplGPT.save_conversation(filepath)
+
+Saves the output of [`ReplGPT.conversation_as_string()`](@ref) to a file at 
+`filepath`. See the chat and output of ReplGPT.save_conversation() below.
+
+## Example:
+
+```julia
+julia> 
+
+ChatGPT> What does LISP stand for in computing?
+  LISP stands for "LISt Processor".
+
+ChatGPT> How about FORTRAN?
+  FORTRAN stands for "FORmula TRANslation".
+
+julia> ReplGPT.save_conversation("/tmp/convo.txt")
+
+shell> cat /tmp/convo.txt
+You:
+----
+
+What does LISP stand for in computing?
+
+ChatGPT:
+--------
+
+LISP stands for "LISt Processor".
+
+You:
+----
+
+How about FORTRAN?
+
+ChatGPT:
+--------
+
+FORTRAN stands for "FORmula TRANslation".
+```
+"""
+function save_conversation(filepath)
+    s = conversation_as_string()
+
+    try
+        open(filepath, "w") do io
+            println(io, s)
+        end
+    catch
+        @error "Failed to open file '$filepath'!"
+    end
+end
 
 function call_chatgpt(s)
     key = getAPIkey()
